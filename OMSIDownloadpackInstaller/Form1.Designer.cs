be3a76f38386000e05a82256e318896eeeb6aeee
Townson, Daniel J <djt4@hw.ac.uk>
2022-06-18 23:32:08 +0100

    Added search and load of installed maps and downloadpack addons

--- OMSIDownloadpackInstaller/Form1.Designer.cs
+++ OMSIDownloadpackInstaller/Form1.Designer.cs
@@ -32,10 +32,19 @@ namespace OMSIDownloadpackInstaller
             this.label1 = new System.Windows.Forms.Label();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.directoryTextBox = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.listBoxDownloadpack = new System.Windows.Forms.CheckedListBox();
+            this.listBoxMaps = new System.Windows.Forms.CheckedListBox();
+            this.buttonLoad = new System.Windows.Forms.Button();
+            this.buttonInstall = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
@@ -50,6 +59,8 @@ namespace OMSIDownloadpackInstaller
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.listBoxDownloadpack);
+            this.groupBox1.Controls.Add(this.label3);
             this.groupBox1.Location = new System.Drawing.Point(13, 150);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(353, 597);
@@ -66,12 +77,12 @@ namespace OMSIDownloadpackInstaller
             this.label2.TabIndex = 2;
             this.label2.Text = "OMSI Directory: ";
             // 
-            // textBox1
+            // directoryTextBox
             // 
-            this.textBox1.Location = new System.Drawing.Point(98, 101);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(332, 20);
-            this.textBox1.TabIndex = 3;
+            this.directoryTextBox.Location = new System.Drawing.Point(98, 101);
+            this.directoryTextBox.Name = "directoryTextBox";
+            this.directoryTextBox.Size = new System.Drawing.Size(332, 20);
+            this.directoryTextBox.TabIndex = 3;
             // 
             // button1
             // 
@@ -81,12 +92,14 @@ namespace OMSIDownloadpackInstaller
             this.button1.TabIndex = 4;
             this.button1.Text = "Browse...";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // groupBox2
             // 
+            this.groupBox2.Controls.Add(this.buttonLoad);
             this.groupBox2.Controls.Add(this.label1);
             this.groupBox2.Controls.Add(this.button1);
-            this.groupBox2.Controls.Add(this.textBox1);
+            this.groupBox2.Controls.Add(this.directoryTextBox);
             this.groupBox2.Controls.Add(this.label2);
             this.groupBox2.Location = new System.Drawing.Point(12, 12);
             this.groupBox2.Name = "groupBox2";
@@ -95,17 +108,88 @@ namespace OMSIDownloadpackInstaller
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "groupBox2";
             // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(45, 16);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(84, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Downloadpacks";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.listBoxMaps);
+            this.groupBox3.Controls.Add(this.label4);
+            this.groupBox3.Location = new System.Drawing.Point(414, 150);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(353, 597);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "groupBox3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(45, 16);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(33, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Maps";
+            // 
+            // listBoxDownloadpack
+            // 
+            this.listBoxDownloadpack.FormattingEnabled = true;
+            this.listBoxDownloadpack.Location = new System.Drawing.Point(9, 61);
+            this.listBoxDownloadpack.Name = "listBoxDownloadpack";
+            this.listBoxDownloadpack.Size = new System.Drawing.Size(338, 529);
+            this.listBoxDownloadpack.TabIndex = 1;
+            // 
+            // listBoxMaps
+            // 
+            this.listBoxMaps.FormattingEnabled = true;
+            this.listBoxMaps.Location = new System.Drawing.Point(9, 57);
+            this.listBoxMaps.Name = "listBoxMaps";
+            this.listBoxMaps.Size = new System.Drawing.Size(338, 529);
+            this.listBoxMaps.TabIndex = 2;
+            // 
+            // buttonLoad
+            // 
+            this.buttonLoad.Location = new System.Drawing.Point(518, 101);
+            this.buttonLoad.Name = "buttonLoad";
+            this.buttonLoad.Size = new System.Drawing.Size(75, 23);
+            this.buttonLoad.TabIndex = 5;
+            this.buttonLoad.Text = "Load";
+            this.buttonLoad.UseVisualStyleBackColor = true;
+            this.buttonLoad.Click += new System.EventHandler(this.buttonLoad_Click);
+            // 
+            // buttonInstall
+            // 
+            this.buttonInstall.Location = new System.Drawing.Point(334, 753);
+            this.buttonInstall.Name = "buttonInstall";
+            this.buttonInstall.Size = new System.Drawing.Size(117, 63);
+            this.buttonInstall.TabIndex = 6;
+            this.buttonInstall.Text = "Install";
+            this.buttonInstall.UseVisualStyleBackColor = true;
+            this.buttonInstall.Click += new System.EventHandler(this.buttonInstall_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1007, 898);
+            this.ClientSize = new System.Drawing.Size(779, 824);
+            this.Controls.Add(this.buttonInstall);
+            this.Controls.Add(this.groupBox3);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.groupBox2.ResumeLayout(false);
             this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -115,9 +199,16 @@ namespace OMSIDownloadpackInstaller
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox directoryTextBox;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.CheckedListBox listBoxDownloadpack;
+        private System.Windows.Forms.CheckedListBox listBoxMaps;
+        private System.Windows.Forms.Button buttonLoad;
+        private System.Windows.Forms.Button buttonInstall;
     }
 }
 
--- OMSIDownloadpackInstaller/Form1.cs
+++ OMSIDownloadpackInstaller/Form1.cs
@@ -3,25 +3,142 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Diagnostics;
 
 namespace OMSIDownloadpackInstaller
 {
     public partial class Form1 : Form
     {
+
+        private string configDirectory = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+        private string OMSIDirectory;
+        private bool validDirectory = false;
+        private string[] invalidMaps = new string[] { "aerials", "Chrono"};
+
         public Form1()
         {
             InitializeComponent();
 
             label1.Font = new Font("Arial", 36, FontStyle.Bold);
-        }
+            label3.Font = new Font("Arial", 22, FontStyle.Bold);
+            label4.Font = new Font("Arial", 22, FontStyle.Bold);
 
+
+
+            if (!(File.ReadAllText(configDirectory) is null))
+            {
+                OMSIDirectory = File.ReadAllText(configDirectory);
+                directoryTextBox.Text = OMSIDirectory;
+                CheckValidDirectory(OMSIDirectory);
+            }
+
+
+        }
+        
         private void label1_Click(object sender, EventArgs e)
         {
 
         }
+
+        private void LoadMaps()
+        {
+            string[] directories = Directory.GetDirectories(OMSIDirectory + @"\maps");
+            foreach (string d in directories)
+            {
+                string folderName = d.Substring(d.IndexOf(@"maps\")+5, (d.Length- (d.IndexOf(@"maps\")+5)));
+                if (!invalidMaps.Contains(folderName))
+                {
+                    listBoxMaps.Items.Add(folderName);
+                }            
+            }
+
+        }
+
+        private void LoadDownloadpacks()
+        {
+            string addonDirectory = OMSIDirectory + @"\Addons\";
+            string[] directories = Directory.GetDirectories(addonDirectory);
+            foreach (string d in directories)
+            {
+                if (d.Contains("Halycon"))
+                {
+                    
+                    string folderName = d.Substring(d.IndexOf("Halycon"), 17);
+                    listBoxDownloadpack.Items.Add(folderName);
+                }
+            }
+        }
+
+        private void CheckValidDirectory(string directory)
+        {
+
+            string[] files = Directory.GetFiles(directory);
+
+            if (files.Any(x => x.Contains("Omsi.exe")))
+            {
+                validDirectory = true;
+            }
+            else
+            {
+                directoryTextBox.Text = "Invalid Directory!";
+                validDirectory = false;
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+
+                    CheckValidDirectory(fbd.SelectedPath);
+                    directoryTextBox.Text = fbd.SelectedPath;
+
+                }
+            }
+
+        }
+
+        private void buttonLoad_Click(object sender, EventArgs e)
+        {
+            if (validDirectory)
+            {
+                LoadDownloadpacks();
+                LoadMaps();
+            }
+        }
+
+        private void buttonInstall_Click(object sender, EventArgs e)
+        {
+            foreach (var addon in listBoxDownloadpack.CheckedItems)
+            {
+
+                Debug.WriteLine(addon.ToString());
+
+                foreach (var map in listBoxMaps.CheckedItems)
+                {
+                    Debug.WriteLine("Installed " + addon.ToString() + " to " + map.ToString());
+                }
+
+            }
+
+        }
+
+        private string GetAddonData()
+        {
+            return null;
+        }
+
+
     }
 }
